f974b5e076ceded4fe314f4fa98582a3b3e76961
Raphael Cuba <raphael@raphaelcuba.com>
2015-09-21 00:53:10 -0300

    Corrige routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  resources :feedbacks
-  resources :buses
-  resources :buses
   root 'home#index'
 
   resources :companies, defaults: {format: :json} do
     resources :buses
   end
 
-  resources :feedbacks, :users
+  resources :feedbacks, :buses
 end
